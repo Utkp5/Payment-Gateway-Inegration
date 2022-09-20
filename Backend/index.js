a5fd9196fdfab4f370014cff3e3e555ba24ea459
Utkp5 <utkp200@gmail.com>
2022-09-20 17:10:27 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv')
 const app = express();
 const PORT = 5000;
+
+
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+
+
 
 app.get("/", function(req,res) {
     return res.send("all done");
