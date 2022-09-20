626e5548e908d1232a19832da311695d04147f2d
Utkp5 <utkp200@gmail.com>
2022-09-20 18:51:16 +0530

    frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
 
 	const initPayment = (data) => {
 		const options = {
-			key: "rzp_test_KUEKfgAtE2mA4H",
+			key: process.env.KEY_ID,
 			amount: data.amount,
 			currency: data.currency,
 			name: book.name,
